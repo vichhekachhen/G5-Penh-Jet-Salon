3f2fb530984909e50be8df3c5bb0bf0a2aca7bdc
khav9 <khav.saroeun@student.passerellesnumeriques.org>
2024-06-21 20:18:01 +0700

    [feat] service api GPJS-26

---
+++ backend/database/migrations/2024_06_21_115726_create_services_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->string('service_name');
+            $table->text('description')->nullable();
+            $table->decimal('price',8,2);
+            $table->integer('duration');
+            $table->timestamps();
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
