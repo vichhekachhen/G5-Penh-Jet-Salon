b17755c74a393ff59faab8af612289f0da183f8a
Khav9 <khav.saroeun@student.passerellesnumeriques.org>
2024-07-06 22:40:29 +0700

    create api for comment and reply GPJS-78

--- backend/app/Models/Reply.php
+++ backend/app/Models/Reply.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reply extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'comment_id', 'owner_id', 'text','images'
