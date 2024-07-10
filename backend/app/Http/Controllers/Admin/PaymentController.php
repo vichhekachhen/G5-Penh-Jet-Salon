1136df315baa4701c8d09ba129139fdf84b75771
VICHHEKA.CHHEN <VICHHEKA.CHHEN@PNC.LAN>
2024-07-10 08:09:32 +0700

    payment to admin

---
+++ backend/app/Http/Controllers/Admin/PaymentController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Payment;
+use Illuminate\Http\Request;
+use App\Models\Province;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+        $this->middleware('role_or_permission:Payment access|Payment create|Payment edit|Payment delete', ['only' => ['index', 'show']]);
+        $this->middleware('role_or_permission:Payment create', ['only' => ['create', 'store']]);
+        $this->middleware('role_or_permission:Payment edit', ['only' => ['edit', 'update']]);
+        $this->middleware('role_or_permission:Payment delete', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::paginate(4);
+        // $payments = Payment::all();
+
+        return view('category.index', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('payment.new');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        // Validate the request including the image
+        $data = $request->validate([
+            'name' => 'required|string|unique:categories,name',
+        ]);
+
+        Category::create($data);
+
+        // Redirect back with a success message
+        return redirect('/admin/categories')->with('success', 'Category created successfully !!!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('category.edit', ['category' => $category]);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $rules = [
+            'name' => 'required|string',
+        ];
+        if ($request->name !== $category->name) {
+            $rules['name'] .= '|unique:categories,name';
+        }
+        $data = $request->validate($rules);
+        $category->update($data);
+
+        return redirect('/admin/categories')->with('success', 'Category updated successfully !!!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->back()->withSuccess('Category deleted !!!');
+    }
+}
