e9105ba7d21806110189af9c207211e3bcd249cd
Khav9 <khav.saroeun@student.passerellesnumeriques.org>
2024-07-10 10:33:51 +0700

    register-owner

--- backend/app/Http/Controllers/Front/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Front/Auth/RegisteredUserController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers\Front\Auth;
 
+use App\Enums\Gender;
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\Frontuser;
+use App\Models\Province;
+use App\Models\Store;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules;
+use Spatie\Permission\Contracts\Role;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +26,8 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        $provinces = Province::all();
+        return view('auth.register', compact('provinces'));
     }
 
     /**
@@ -37,13 +44,49 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'shop_name' => ['required', 'string'],
+            'province_id' => ['required', 'integer'],
+            'city' => ['required', 'string'],
+            'gender' => ['required', 'in:male,female,other'],
+            'shop_profile' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+
+        // Handle the file upload for shop_profile
+        if ($request->hasFile('shop_profile')) {
+            $path = $request->file('shop_profile')->store('public/StoreImages');
+            $shopProfileUrl = Storage::url($path);
+        } else {
+            $shopProfileUrl = null;
+        }
+
+        // Create the address
+        $address = Address::create([
+            'city' => $request->city,
+            'province_id' => $request->province_id,
+        ]);
+
+        // Create the store
+        $store = Store::create([
+            'shop_name' => $request->shop_name,
+            'shop_profile' => $shopProfileUrl,
+            'address_id' => $address->id,
+        ]);
+
+
 
         $user = Frontuser::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'gender' => Gender::from($request->gender),
+            'store_id' => $store->id,
         ]);
+
+        // Assign role to the user
+        $ownerRole = Role::where('name', 'owner')->first();
+        if ($ownerRole) {
+            $user->assignRole($ownerRole);
+        }
 
         event(new Registered($user));
 
--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -1,66 +1,62 @@
 <x-guest-layout>
     <div class="font-sans min-h-screen antialiased bg-gray-900 pt-6 pb-5">
-        <div class="flex flex-col justify-center max-w-6xl sm:m-auto mx-5 mb-5 space-y-5">
-            <h1 class="font-bold text-center text-4xl text-yellow-500">Penh Jet<span class="text-blue-500"> Salon</span></h1>
+        <div class="flex flex-col justify-center max-w-4xl sm:m-auto mx-5 mb-5 space-y-5">
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
             <!-- Validation Errors -->
             <x-auth-validation-errors class="mb-4" :errors="$errors" />
-            <form method="POST" action="{{ route('admin.login') }}">
-                @csrf
-                <div class="grid grid-cols-6 gap-6 bg-white p-6">
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="text" name="service_name" id="service_name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="Enter Service Name" required="">
-                    </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="email" name="email" id="email" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Email" :value="old('email')" required autofocus />
+            <div class="flex flex-col justify-center font-[sans-serif] p-4 ">
+                <div class="max-w-md w-full mx-auto shadow-[0_2px_10px_-2px_rgba(195,169,50,0.5)] p-8 relative mt-12">
+                    <div class="bg-white w-24 h-24 border-[10px] p-1.5 absolute left-0 right-0 mx-auto -top-12 rounded-full overflow-hidden">
+                        <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui-short.svg" alt="logo" class='w-full inline-block' />
+                        </a>
                     </div>
 
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="password" name="password" id="password" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Password" required autocomplete="current-password" />
-                    </div>
+                    <form class="mt-12 bg-white p-8" method="POST" action="{{ route('admin.register') }}" enctype="multipart/form-data">
+                        @csrf
+                        <h3 class="text-xl font-bold text-orange-500 mb-8 text-center">Create free account</h3>
+                        <div class="space-y-3">
+                            <input name="name" id="name" type="text" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Username" />
+                            <input name="email" type="text" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Enter email" />
+                            <input name="password" type="password" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Enter password" />
+                            <input name="password_confirmation" id="password_confirmation" type="password" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Enter confirm password" />
+                            <label for="" class="text-gray-500 text-sm">Birhtday</label>
+                            <input name="birth" type="date" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Enter confirm password" />
+                            <select id="province_id" name="province_id" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all">
+                                <option selected disabled>Select Province</option>
+                                @foreach($provinces as $key => $province)
+                                <option value="{{$province->id}}">{{$province->province_name}}</option>
+                                @endforeach
+                            </select>
+                            <input name="shop_name" id="shop_name" type="text" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Shop Name" />
+                            <input name="city" id="city" type="text" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Location Of Shop" />
+                            <label for="qr_code" class="text-gray-500 text-sm">Upload QR Code:</label>
+                            <input name="qr_code" type="file" class="bg-gray-100 w-full text-sm text-gray-800 px-4 py-2 focus:bg-transparent outline-orange-300 transition-all" placeholder="Enter confirm password" />
+                            <div class="flex items-center">
+                                <input name="gender" type="radio" value="male" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <label for="remember-me" class="ml-3 block text-sm text-gray-800">Male<a href="javascript:void(0);" class="text-orange-500 font-semibold hover:underline ml-1"></a>
+                                </label>
+                                <input name="gender" value="female" type="radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <label for="remember-me" class="ml-3 block text-sm text-gray-800">Female<a href="javascript:void(0);" class="text-orange-500 font-semibold hover:underline ml-1"></a>
+                                </label>
+                                <input name="gender" type="radio" value="other" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <label for="remember-me" class="ml-3 block text-sm text-gray-800">Other<a href="javascript:void(0);" class="text-orange-500 font-semibold hover:underline ml-1"></a>
+                                </label>
+                            </div>
+                        </div>
 
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="password" name="password" id="password" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Comfirm Password" required autocomplete="current-password" />
-                    </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="text" name="discount" id="discount" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="Shop Name" required="">
-                    </div>
-                    <!-- <div class="col-span-6 sm:col-span-3">
-                        <input type="number" name="duration" id="duration" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="$2300" required="">
-                    </div> -->
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="file" name="image" id="image" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="$2300" required="">
-                    </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <input type="file" name="image" id="image" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="$2300" required="">
-                    </div>
+
+
+                        <div class="mt-8">
+                            <button type="submit" class="w-full py-4 px-8 text-sm tracking-wide font-semibold text-white bg-blue-500 hover:bg-orange-600 focus:outline-none">
+                                Create an account
+                            </button>
+                        </div>
+                        <p class="text-sm mt-8 text-center text-gray-800">Already have an account? <a href="javascript:void(0);" class="text-orange-500 font-semibold hover:underline ml-1">Login here</a></p>
+                    </form>
                 </div>
-                <!-- <div class="flex flex-col bg-white p-10 rounded-lg shadow space-y-6">
-              <h1 class="font-bold text-xl text-center">Sign in to your account</h1>
-
-              <div class="flex flex-col space-y-1">
-                <input type="email" name="email" id="email" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Email" :value="old('email')" required autofocus />
-              </div>
-
-              <div class="flex flex-col">
-                <input type="password" name="password" id="password" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Password" required autocomplete="current-password"/>
-              </div>
-              <div class="flex flex-col-reverse sm:flex-row sm:justify-between items-center">
-                <button type="submit" class=" w-full bg-blue-500 text-white font-bold px-5 py-2 rounded focus:outline-none shadow hover:bg-blue-700 transition-colors m-auto">Log In</button>
-              </div>
-              <div class="flex flex-col-reverse sm:flex-row sm:justify-between items-center mt-10">
-                <span>Already registered? <a href="/" class="text-blue-600">Sign In</a></span>
-              </div>
-            </div> -->
-            </form>
-            <div class="flex justify-center text-gray-500 text-sm">
-                <p>Copyright by Penh Jet Salon <script>
-                        document.write(new Date().getFullYear());
-                    </script>
-                </p>
             </div>
         </div>
-    </div>
+
 
 </x-guest-layout>
