eb07c4ffe7e9a543b9863f84592ce274de0d70a8
khav9 <khav.saroeun@student.passerellesnumeriques.org>
2024-07-13 16:55:23 +0700

    map layout

---
+++ frontend/src/Components/map/MapCom.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Map</h1>
+    <body class="flex items-center justify-center h-screen bg-gray-100">
+      <div class="bg-white p-4 rounded shadow-md flex items-center space-x-2">
+        <input
+          type="text"
+          placeholder="Enter Your Address"
+          class="border border-gray-300 p-2 rounded flex-grow"
+        />
+        <button class="bg-red-500 text-white px-4 py-2 rounded">Go</button>
+      </div>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
+
