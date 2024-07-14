078747b0560b8a5fa6f3c6c2e158b01c1be4fa78
LEY.TEM <LEY.TEM@PNC.LAN>
2024-07-14 12:04:33 +0700

    pust

--- frontend/src/Components/UserPage/History/HistoryBooking.vue
+++ frontend/src/Components/UserPage/History/HistoryBooking.vue
@@ -23,13 +23,49 @@
               <td>{{ booking.date }}</td>
               <td>{{ booking.time }}</td>
               <td>{{ booking.total_price }}</td>
-              <td class="text-blue">{{ booking.status }}</td>
+              <td class="text-blue" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
     <!-- {{ store.bookings }} -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel">
+      <div class="modal-dialog">
+        <div class="modal-content" style="width: 120vh; margin:20px;">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Booking Details</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div>
+              <div class="table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th scope="col">Shop Name</th>
+                      <th scope="col">Service Name</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Pricie</th>
+                      <th scope="col">Duration</th>
+                      <th scope="col">Discount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </WebLayout>
 </template>
 
