2b2b8b8f7878faa7cc3e74e0f573d2942ee85977
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-07-18 14:13:55 +0700

    [ipm] update search shop

--- frontend/src/Components/UserPage/Shoplist/ListShopComponent.vue
+++ frontend/src/Components/UserPage/Shoplist/ListShopComponent.vue
@@ -3,12 +3,34 @@
     <!-- <img class="w-100 h-95" src="../../../Images/shop1.jpg" alt="Image"> -->
     <section>
       <div class="container px-4 px-lg-5 mt-3 pt-3">
-        <input
-          class="form-control"
-          type="search"
-          v-model="searchQuery"
-          placeholder="Search for a shop"
-        />
+        <div class="input-group mb-3">
+          <!-- search Shop -->
+          <div class="relative md:w-auto w-50 md:mt-0">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-5 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="search-navbar"
+              v-model="searchQuery"
+              @input="searchService"
+              class="block w-full p-2 pl-10 text-sm text-blue-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
         <div class="my-3 px-lg-2 flex d-flex align-items-center pb-3 pt-3">
           <router-link to="/" class="nav-link my-3 text-xl font-bold">Home ></router-link>
           <div class="text-lg font-bold text-gray-400">{{ provinceName }}</div>
